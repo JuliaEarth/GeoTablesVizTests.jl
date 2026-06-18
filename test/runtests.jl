6b2ba3f018909169472ff61f276d51a5fe3d9da2
Júlio Hoffimann <julio.hoffimann@gmail.com>
2026-06-18 14:49:01 -0300

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,9 +6,9 @@ using TestItemRunner
 @testsnippet Setup begin
   using GeoTables
   using Meshes
-  using CoordRefSystems
-  using Distributions
   using Unitful
+  using Distributions
+  using CoDa
   using Colors
   using ReferenceTests
   using StableRNGs
@@ -31,14 +31,6 @@ end
   a = rand(rng, 1:5, 100)
   gtb = georef((; a), grid)
   @test_reference joinpath(datadir, "integer.png") viewer(gtb)
-
-  a = fill(1.0, 100)
-  gtb = georef((; a), grid)
-  @test_reference joinpath(datadir, "const-float.png") viewer(gtb)
-
-  a = fill(1, 100)
-  gtb = georef((; a), grid)
-  @test_reference joinpath(datadir, "const-integer.png") viewer(gtb)
 end
 
 @testitem "categorical" setup = [Setup] begin
@@ -47,10 +39,6 @@ end
   a = rand(rng, ["yes", "no"], 100)
   gtb = georef((; a), grid)
   @test_reference joinpath(datadir, "categorical.png") viewer(gtb)
-
-  a = fill("test", 100)
-  gtb = georef((; a), grid)
-  @test_reference joinpath(datadir, "const-categorical.png") viewer(gtb)
 end
 
 @testitem "unitful" setup = [Setup] begin
@@ -63,14 +51,6 @@ end
   a = rand(rng, 1:5, 100) * u"°C"
   gtb = georef((; a), grid)
   @test_reference joinpath(datadir, "unit-integer.png") viewer(gtb)
-
-  a = fill(1.0 * u"m/s", 100)
-  gtb = georef((; a), grid)
-  @test_reference joinpath(datadir, "const-unit-float.png") viewer(gtb)
-
-  a = fill(1 * u"°C", 100)
-  gtb = georef((; a), grid)
-  @test_reference joinpath(datadir, "const-unit-integer.png") viewer(gtb)
 end
 
 @testitem "distributional" setup = [Setup] begin
@@ -80,12 +60,29 @@ end
   gtb = georef((; a), grid)
   @test_reference joinpath(datadir, "normal.png") viewer(gtb)
 
-  a = fill(Normal(), 100)
+  a = Dirac.(rand(rng, 100))
   gtb = georef((; a), grid)
-  @test_reference joinpath(datadir, "const-normal.png") viewer(gtb)
+  @test_reference joinpath(datadir, "dirac.png") viewer(gtb)
+
+  a = Bernoulli.(rand(rng, 100))
+  gtb = georef((; a), grid)
+  @test_reference joinpath(datadir, "bernoulli.png") viewer(gtb)
+
+  a = Categorical.([rand(rng, Dirichlet([1/3, 1/3, 1/3])) for _ in 1:100])
+  gtb = georef((; a), grid)
+  @test_reference joinpath(datadir, "categdist.png") viewer(gtb)
 end
 
 @testitem "compositional" setup = [Setup] begin
+  rng = StableRNG(123)
+
+  a = rand(rng, Composition{2}, 100)
+  gtb = georef((; a), grid)
+  @test_reference joinpath(datadir, "compositional-1.png") viewer(gtb)
+
+  a = rand(rng, Composition{3}, 100)
+  gtb = georef((; a), grid)
+  @test_reference joinpath(datadir, "compositional-2.png") viewer(gtb)
 end
 
 @testitem "colorful" setup = [Setup] begin
@@ -94,10 +91,6 @@ end
   a = Gray.(rand(rng, 100))
   gtb = georef((; a), grid)
   @test_reference joinpath(datadir, "colorful.png") viewer(gtb)
-
-  a = fill(Gray(0.1), 100)
-  gtb = georef((; a), grid)
-  @test_reference joinpath(datadir, "const-colorful.png") viewer(gtb)
 end
 
 @testitem "invalid" setup = [Setup] begin
@@ -120,7 +113,7 @@ end
   gtb = georef((; a), grid)
   @test_reference joinpath(datadir, "invalid-integer.png") viewer(gtb)
 
-  a = shuffle(rng, [missings; fill("test", 20); rand(rng, ["yes", "no"], 60)])
+  a = shuffle(rng, [missings; rand(rng, ["yes", "no"], 80)])
   gtb = georef((; a), grid)
   @test_reference joinpath(datadir, "invalid-categorical-1.png") viewer(gtb)
 
@@ -140,7 +133,45 @@ end
   gtb = georef((; a), grid)
   @test_reference joinpath(datadir, "invalid-distributional.png") viewer(gtb)
 
+  a = shuffle(rng, [missings; rand(rng, Composition{3}, 80)])
+  gtb = georef((; a), grid)
+  @test_reference joinpath(datadir, "invalid-compositional.png") viewer(gtb)
+
   a = shuffle(rng, [missings; RGB.(rand(rng, 80), rand(rng, 80), rand(rng, 80))])
   gtb = georef((; a), grid)
   @test_reference joinpath(datadir, "invalid-colorful.png") viewer(gtb)
+end
+
+@testitem "constant" setup = [Setup] begin
+  a = fill(1.0, 100)
+  gtb = georef((; a), grid)
+  @test_reference joinpath(datadir, "const-float.png") viewer(gtb)
+
+  a = fill(1, 100)
+  gtb = georef((; a), grid)
+  @test_reference joinpath(datadir, "const-integer.png") viewer(gtb)
+
+  a = fill("test", 100)
+  gtb = georef((; a), grid)
+  @test_reference joinpath(datadir, "const-categorical.png") viewer(gtb)
+
+  a = fill(1.0 * u"m/s", 100)
+  gtb = georef((; a), grid)
+  @test_reference joinpath(datadir, "const-unit-float.png") viewer(gtb)
+
+  a = fill(1 * u"°C", 100)
+  gtb = georef((; a), grid)
+  @test_reference joinpath(datadir, "const-unit-integer.png") viewer(gtb)
+
+  a = fill(Normal(), 100)
+  gtb = georef((; a), grid)
+  @test_reference joinpath(datadir, "const-distributional.png") viewer(gtb)
+
+  a = fill(Composition(0.2, 0.5, 0.3), 100)
+  gtb = georef((; a), grid)
+  @test_reference joinpath(datadir, "const-compositional.png") viewer(gtb)
+
+  a = fill(Gray(0.1), 100)
+  gtb = georef((; a), grid)
+  @test_reference joinpath(datadir, "const-colorful.png") viewer(gtb)
 end
